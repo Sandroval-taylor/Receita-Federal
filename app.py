00594d9ee31089dc200c8e293f7cf98c3f7280e8
Sandroval Ribeiro Taylor <contato@rommel.adv.br>
2024-12-15 14:36:14 -0300

    sandrocorrecao

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template
 from flasgger import Swagger
 from flask_cors import CORS
-from API import api_pesquisa
-from export_excel import export_excel
-from unificar import process_excel
+from receita import api_pesquisa
+from exportarExcel import export_excel
+from unificarEscProc import process_excel
 import config
 
 # Inicialização da aplicação Flask
@@ -33,12 +33,12 @@ def index():
     return render_template("index.html", **config.template())# Renderiza a página com variáveis zeradas
 
 # Rotas da API
-app.add_url_rule("/API", "API", api_pesquisa, methods=["POST"])
+app.add_url_rule("/receita", "API", api_pesquisa, methods=["POST"])
 
 # Rotas de exportação e processamento
-app.add_url_rule("/export_excel", "export_excel", export_excel, methods=["GET"])
-app.add_url_rule("/process_excel", "unificar", process_excel, methods=["POST"])
+app.add_url_rule("/exportarExcel", "export_excel", export_excel, methods=["GET"])
+app.add_url_rule("/unificarEscProc", "unificar", process_excel, methods=["POST"])
 
 # Inicialização do servidor
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001)
+    app.run(host="0.0.0.0", port=5000)
